03305dbe54e49d735a7707019cddc370ccc2b1b7
Rafael Albuquerque <rafael.albuquerque.it@gmail.com>
2021-06-20 19:35:46 -0300

    City - deleteShouldReturnNoContentWhenIndependentId test passed

--- bds02/src/main/java/com/ralbuquerque/bds02/services/CityService.java
+++ bds02/src/main/java/com/ralbuquerque/bds02/services/CityService.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.ralbuquerque.bds02.dto.CityDTO;
 import com.ralbuquerque.bds02.entities.City;
 import com.ralbuquerque.bds02.repositories.CityRepository;
+import com.ralbuquerque.bds02.services.exceptions.DatabaseException;
+import com.ralbuquerque.bds02.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CityService {
@@ -21,4 +25,17 @@ public class CityService {
 		List<City> list = repository.findAll(Sort.by("name"));
 		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
 	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+	
 }
